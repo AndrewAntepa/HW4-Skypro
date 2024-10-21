8bd62476f159e819637120d8fd9a37ef53c71a75
AndrewAntepa <andrewantepa@gmail.com>
2024-10-21 22:30:15 +0800

    hw4

--- src/Main.java
+++ src/Main.java
@@ -6,59 +6,85 @@ public class Main {
 
         System.out.println("Задание 1\nВведите возраст: ");
         int age = in.nextInt();
-        if (age < 18)
+        if (age < 18) {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать\n");
-        else System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний\n");
+        }
+        else {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний\n");
+        }
 
         System.out.println("Задание 2\nСколько градусов? ");
         int temperature = in.nextInt();
-        if(temperature < 5)
+        if(temperature < 5) {
             System.out.println("На улице " + temperature + " градусов, нужно надеть шапку\n");
-        else System.out.println("На улице " + temperature + " градусов, можно идти без шапки\n");
+        }
+        else{
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки\n");
+        }
 
         System.out.println("Задание 3\nСкорость равно: ");
         int speed = in.nextInt();
-        if (speed > 60)
+        if (speed > 60) {
             System.out.println("Если скорость " + speed + ", то  придется заплатить штраф\n");
-        else System.out.println("Если скорость " + speed + ", то можно ездить спокойно\n");
+        }
+        else{
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно\n");
+        }
 
         System.out.println("Задание 4\nСколько лет человеку?");
         int ageForPlace = in.nextInt();
-        if(ageForPlace >= 2 && ageForPlace <= 6)
+        if(ageForPlace >= 2 && ageForPlace <= 6) {
             System.out.println("Если возраст человека равен " + ageForPlace + ", то ему нужно ходить в детский сад\n");
-        else if (ageForPlace <= 17)
+        }
+        else if (ageForPlace <= 17) {
             System.out.println("Если возраст человека равен " + ageForPlace + ", то ему нужно ходить в школу\n");
-        else if(ageForPlace <= 24)
+        }
+        else if(ageForPlace <= 24) {
             System.out.println("Если возраст человека равен " + ageForPlace + ", то ему нужно ходить в университет\n");
-        else System.out.println("Если возраст человека равен " + ageForPlace + ", то ему пора уже работать\n");
+        }
+        else{
+            System.out.println("Если возраст человека равен " + ageForPlace + ", то ему пора уже работать\n");
+        }
 
         System.out.println("Задание 5\nСколько лет ребенку?");
         int childAgeForAttraction = in.nextInt();
-        if(childAgeForAttraction < 5)
+        if(childAgeForAttraction < 5) {
             System.out.println("Если возраст ребенка равен " + childAgeForAttraction + ", то ему нельзя кататься на аттракционе\n");
-        else if(childAgeForAttraction <= 14)
+        }
+        else if(childAgeForAttraction <= 14) {
             System.out.println("Если возраст ребенка равен " + childAgeForAttraction + ", то ему можно кататься на аттракционе в сопровождении\n");
-        else System.out.println("Если возраст ребенка равен " + childAgeForAttraction + ", то ему можно без сопровождения взрослого\n");
+        }
+        else{
+            System.out.println("Если возраст ребенка равен " + childAgeForAttraction + ", то ему можно без сопровождения взрослого\n");
+        }
 
         System.out.println("Задание 6\nСколько человек в автобусе?");
         int placesInBus = in.nextInt();
-        if(placesInBus < 60)
+        if(placesInBus < 60) {
             System.out.println("В автобусе есть сидячие места\n");
-        else if(placesInBus < 102)
+        }
+        else if(placesInBus < 102) {
             System.out.println("В автобусе нет сидячих мест, но есть стоячие\n");
-        else System.out.println("В автобусе нет мест\n");
+        }
+        else{
+            System.out.println("В автобусе нет мест\n");
+        }
 
         System.out.println("задание 7\nВведите 3 числа:");
         int one = in.nextInt();
         int two = in.nextInt();
         int three = in.nextInt();
-        if(one > two && one > three)
+        if(one > two && one > three) {
             System.out.println("Число " + one + " самое большое");
-        else if (two > one && two > three)
+        }
+        else if (two > one && two > three) {
             System.out.println("Число " + two + " самое большое");
-        else if(three > one && three > two)
+        }
+        else if(three > one && three > two) {
             System.out.println("Число " + three + " самое большое");
-        else if(one == two && two == three)
+        }
+        else if(one == two && two == three) {
             System.out.println("Числа равны");
+        }
     }
 }
